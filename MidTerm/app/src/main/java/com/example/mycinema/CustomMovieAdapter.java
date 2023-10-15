0c9677e5c29974dda5d4dc64c5ecad21117f5c67
Astero1dz <nhockrockspx@gmail.com>
2023-10-16 01:28:59 +0700

    fixing the imageView in Booking and the name of films

--- MidTerm/app/src/main/java/com/example/mycinema/CustomMovieAdapter.java
+++ MidTerm/app/src/main/java/com/example/mycinema/CustomMovieAdapter.java
@@ -1,10 +1,13 @@
 package com.example.mycinema;
 
+import static com.example.mycinema.MainActivity.movieList;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.ArrayList;
@@ -19,9 +22,21 @@ public class CustomMovieAdapter extends ArrayAdapter<Movie> {
     public View getView(int position, View convertView, ViewGroup parent) {
         Movie movie = getItem(position);
 
+
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_cell, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_cell_favorite_list, parent, false);
         }
+        Button removeButton = convertView.findViewById(R.id.removeButton);
+        removeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Movie selectedMovie = movieList.get(position);
+               ArrayList<Movie> favoriteMovie = DetailActivity.getFavoriteMovies();
+              favoriteMovie.remove(selectedMovie.getId().toString());
+               notifyDataSetChanged();
+            }
+
+        });
 
         ImageView imageView = convertView.findViewById(R.id.movieImage);
         TextView textView = convertView.findViewById(R.id.movieName);
