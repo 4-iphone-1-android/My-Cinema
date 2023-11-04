5f0b2571a2ac49cae02808ded47e684eaf118ec1
DanteAdam <tuannguyenduy2@gmail.com>
2023-11-04 17:32:41 +0700

    add google services and implement library in build.gradle.kts

--- MidTerm/app/build.gradle.kts
+++ MidTerm/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -65,4 +66,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
