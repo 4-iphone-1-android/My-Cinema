86eabdee6565124b347b879ec36ab81ec7ed6e86
sonyway <baotoantrinh2002@gmail.com>
2023-11-21 13:21:02 +0700

    - Modify ProfileActivity.java and the layout - Add ChangePasswordActivity, DeleteAccountActivity

--- MidTerm/app/src/main/java/com/example/mycinema/ChangePasswordActivity.java
+++ MidTerm/app/src/main/java/com/example/mycinema/ChangePasswordActivity.java
@@ -1,14 +1,126 @@
 package com.example.mycinema;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Pattern;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+    private Button btnSendEmail;
+    private EditText etEmail;
+    private FirebaseAuth mAuth;
+    private final static String TAG = "ChangePasswordActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.pink)));
+            actionBar.setTitle("Change Password");
+        }
+
+        btnSendEmail = findViewById(R.id.btnSendEmail);
+        etEmail = findViewById(R.id.etEmail);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                if (TextUtils.isEmpty(email)) {
+                    etEmail.setError("Email is required");
+                    etEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    etEmail.setError("Invalid email");
+                    etEmail.requestFocus();
+                } else {
+                    resetPassword(email);
+                }
+            }
+        });
+    }
+
+    private void resetPassword(String email) {
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    if (user != null && user.isEmailVerified()) {
+                        Toast.makeText(ChangePasswordActivity.this, "Password reset email sent.", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        etEmail.setError("Email is not registered");
+                        etEmail.requestFocus();
+                    }
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (Exception e) {
+                        Log.e(TAG, "Password reset failed: " + e.getMessage());
+                        Toast.makeText(ChangePasswordActivity.this, "Password reset failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int UID = item.getItemId();
+        if (UID == R.id.menu_update_profile) {
+            Intent intent = new Intent(this, UpdateProfileActivity.class);
+            startActivity(intent);
+        } else if (UID == R.id.menu_change_password) {
+            Intent intent = new Intent(this, ChangePasswordActivity.class);
+            startActivity(intent);
+        } else if (UID == R.id.menu_delete_account) {
+            Intent intent = new Intent(this, DeleteAccountActivity.class);
+            startActivity(intent);
+        } else if (UID == R.id.menu_logout) {
+            logoutUser();
+        } else if (UID == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logoutUser() {
+        mAuth.signOut();
+        Intent mainIntent = new Intent(this, LoginActivity.class);
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
